feb4031278c328a6a495748c839f051f38198b4c
Sócrates Lopes <lopes.socrates@hotmail.com>
2013-10-03 17:36:16 -0300

    Proxy file was not included in previous commit.

---
+++ Structural/Proxy/Proxy/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using Proxy.ExampleService;
+
+namespace Proxy
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Class Reference.cs is the proxy
+            ServiceClient serviceProxy = new ServiceClient();
+
+            var getDataResult = serviceProxy.GetData(1);
+            Console.WriteLine("GetData() returned: {0}", getDataResult);
+
+
+            var getDataUsingDataContractResult = serviceProxy.GetDataUsingDataContract(new CompositeType
+             {
+                 BoolValue = true,
+                 ExtensionData = null,
+                 StringValue = "Hello Composite"
+             });
+
+            Console.WriteLine("GetDataUsingDataContract() returned: {0}",
+                getDataUsingDataContractResult.StringValue);
+
+            Console.ReadKey();
+        }
+    }
+}
