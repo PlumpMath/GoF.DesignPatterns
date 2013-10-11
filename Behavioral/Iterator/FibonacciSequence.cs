f81754934bf98d0d7b479086bdb03d706adf6fcf
Sócrates Lopes <lopes.socrates@hotmail.com>
2013-10-11 18:07:56 -0300

    Added Iterator pattern.

---
+++ Behavioral/Iterator/FibonacciSequence.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+
+namespace Iterator
+{
+    public class FibonacciSequence : IEnumerable<int>
+    {
+        public int NumberOfValues { get; set; }
+
+        public FibonacciSequence(int numberOfValues)
+        {
+            NumberOfValues = numberOfValues;
+        }
+
+        public IEnumerator<int> GetEnumerator()
+        {
+          return new FibonacciEnumerator(NumberOfValues);
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator();
+        }
+    }
+}
---
+++ Behavioral/Iterator/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Iterator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var numberOfValues = new Random().Next(5, 50);
+            Console.WriteLine("Generating a Fibonacci sequence with {0} itens.", numberOfValues);
+
+            var sequence = new FibonacciSequence(numberOfValues);
+
+            var enumerator = sequence.GetEnumerator();
+            while (enumerator.MoveNext())
+            {
+                Console.WriteLine(enumerator.Current);
+            }
+
+            Console.ReadKey();
+
+        }
+    }
+}
